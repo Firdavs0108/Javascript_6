c30ad1d138f26e8220e6c0873e381ee58cc1365f
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-03-17 22:43:02 +0900

    first commit

---
+++ task1.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+//=============================== 1
+
+// let str = 'fozil'
+// let NewStr = ' '
+
+// for ( let i= str.length -1; i>=0; i--) {
+//     NewStr += str[i]
+// }
+
+// console.log(NewStr);
+
+
+//================================  2
+
+
+// let str = 'Webbrain  '
+// let str2 = 'ItBrain  '
+
+// console.log(str.repeat(10));
+// console.log(str2.repeat(10));
+// // console.log(str2.replace('Web', 'IT'))
+
+//==================================  3
+
+
+// let str1 = 'Web'
+// let str2 = 'Brain'
+
+// console.log(str1.replace ('Web' ,  'Brain'));
+// console.log(str2.replace ('Brain' ,  'Web'));
+
+//==================================  4
+
+// let str = '#fozil#web#brain#'
+
+// console.log(str.replace('#fozil#web#brain#' , '(fozil)web(brain)' ));
+
+//==================================  5
+
+let str = ' Fozil        '
+let str2 =  'Fozil' 
+
+
+if (str2 == str.trim() ) {
+   console.log('True');
+
+} else{
+    console.log('False')
+}
+
+
